5efd4bb5e738baf1b95e4244131f192ec7b4075e
IndianSensei <trescothickpereira@gmail.com>
2024-01-25 22:23:30 +0000

    Database Intergration

---
+++ Database-Intergration.py
@@ -0,0 +1,40 @@
+import CTkMessagebox
+import customtkinter as ctk 
+import pyodbc 
+
+conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\Tres\Documents\Car-Rental-Store\Car_Rental.accdb;')
+
+
+
+
+def main():
+    runSQL("SELECT * FROM Login",True)
+
+
+
+def runSQL(strSQL, results):
+    global conn 
+
+    cursor = conn.cursor()
+    cursor.execute(strSQL)
+
+    if results:
+        print('\n')
+
+        columns = [column[0] for column in cursor.description]
+        for c in columns:
+            print(c, end = ",")
+        print("")
+
+        for row in cursor.fetchall():
+            for c in row:
+                print(c, end = "\t")
+        print("")
+
+    else:
+        conn.commit()
+        print("\n", strSQL, ": Run - ", cursor.rowcount, "record(s) affected")
+    
+    cursor.close()
+
+main()
